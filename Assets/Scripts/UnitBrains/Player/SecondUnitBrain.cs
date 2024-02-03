f25077ae2dba57538723076a7f06039974599f1d
andrey kolesnikov <kolesnikov.andrey16@gmail.com>
2024-02-03 13:20:12 +0300

    hw7

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Model;
 using Model.Runtime.Projectiles;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace UnitBrains.Player
@@ -12,43 +16,114 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+        List<Vector2Int> outOfReachTargets = new List<Vector2Int>();
+
+        public static int counter = 0;
+
+        public void UnitNumber()
+        {
+            if (IsTargetInRange(outOfReachTargets(1)))
+            {
+                counter = outOfReachTargets(1);
+            }
+
+            
+        }
+
+        const int MaxTargets = 4;
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
-            ///////////////////////////////////////
+            float temp = GetTemperature();
+
+            if (temp >= overheatTemperature)
+            {
+                return;
+            }
+            IncreaseTemperature();
+
+
+            for (int i = 0; i <= temp; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+
         }
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            if (outOfReachTargets.Any())
+            {
+                if (IsTargetInRange(outOfReachTargets[0]))
+                {
+                    return unit.Pos;
+                }
+                else
+                {
+                    var target = CalcNextStepTowards(outOfReachTargets[outOfReachTargets.Count - 1]);
+                    return target;
+                }
+
+            }
+
+            return unit.Pos;
         }
 
         protected override List<Vector2Int> SelectTargets()
         {
-            ///////////////////////////////////////
-            // Homework 1.4 (1st block, 4rd module)
-            ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+
+            List<Vector2Int> result = new List<Vector2Int>();
+            float Min = float.MaxValue;
+            Vector2Int bestTarget = Vector2Int.zero;
+
+            foreach (Vector2Int i in GetAllTargets())
             {
-                result.RemoveAt(result.Count - 1);
+                float distance = DistanceToOwnBase(i);
+
+                if (distance < Min)
+                {
+                    Min = distance;
+                    bestTarget = i;
+                }
+
             }
+            outOfReachTargets.Clear();
+            if (Min < float.MaxValue)
+            {
+                outOfReachTargets.Add(bestTarget);
+                if (IsTargetInRange(bestTarget))
+                {
+                    result.Add(bestTarget);
+                }
+            }
+            else
+            {
+                if (IsPlayerUnitBrain) outOfReachTargets.Add(runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId]);
+            }
+
+            outOfReachTargets.Clear();
+
+            foreach (Vector2Int i in GetAllTargets())
+            {
+                outOfReachTargets.Add(i);
+
+                if (IsPlayerUnitBrain) outOfReachTargets.Add(runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId]);
+
+                SortByDistanceToOwnBase(outOfReachTargets);
+
+                
+            }
+
             return result;
-            ///////////////////////////////////////
         }
 
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -60,7 +135,7 @@ namespace UnitBrains.Player
 
         private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
 
