02fbadac7c3a8d53bf10bfd8f5f26635d3d00d1e
andrey kolesnikov <kolesnikov.andrey16@gmail.com>
2024-02-25 11:59:27 +0300

    Preparing for PR1

---
+++ Assets/Scripts/UnitBrains/Pathfinding/NewUnitPath.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using UnitBrains;
+using UnitBrains.Pathfinding;
+using UnityEngine;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace Assets.Scripts.UnitBrains.Pathfinding
+{
+        
+    public abstract class NewUnitPath : BaseUnitPath
+    {
+
+        public List<Vector2Int> FindPath()
+        {
+            List<Vector2Int> openList = new List<Vector2Int>();
+            List<Vector2Int> closedList = new List<Vector2Int>();
+                    
+            while (openList.Count > 0)
+            {
+                Vector2Int currentPoint = openList[0];
+
+                foreach (var Vector2Int in openList)                
+                { 
+                    
+                }
+            }
+        }    
+    }
+}
---
+++ Assets/Scripts/UnitBrains/Pathfinding/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.UnitBrains.Pathfinding
+{
+    public class Point
+    {
+        public int X; 
+        public int Y;
+        public int Cost = 10;
+        public int Estimate;
+        public int Value;
+
+        public Point(int x, int y) 
+        { 
+            X = x;
+            Y = y;
+        }
+
+        public void CalcEstimate(int targetX, int targetY)
+        {
+            Estimate = Math.Abs(X - targetX) + Math.Abs(Y - targetY);
+        }
+
+        public void CalcValue()
+        {
+            Value = Cost + Estimate;
+        }
+    }
+}
